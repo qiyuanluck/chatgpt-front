6acfb3471f8bf8588bb6660ddaa817157fb1e972
qiyuan <1184863960@qq.com>
2024-12-28 17:50:05 +0800

    feat: 完善对话处理

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,10 +1,8 @@
-export interface DialogType {
+import {GptVersion} from "@/app/constants";
+
+export interface Dialog {
     // 头像
     avatar: string;
-    // 对话ID
-    dialogId: number;
-    // 是否阅读
-    read: boolean;
     // 小标题
     subTitle: string;
     // 对话最后时间
@@ -17,10 +15,21 @@ export interface DialogType {
 
 export interface Message {
     avatar: string;
-    message: string;
+    content: string;
     message_type: MessageType;
     time: number;
     direction?: MessageDirection;
+    role: MessageRole;
+}
+
+export interface SessionConfig {
+    gptVersion: GptVersion;
+}
+
+export enum MessageRole{
+    system = 0,
+    user = 1,
+    assistant = 2
 }
 
 export enum MessageType {
